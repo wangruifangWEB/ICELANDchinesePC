6ec8064c81068ff35cfc69b305c97edcd89ee8db
wangruifang <wangruifang1001@sina.cn>
2019-04-08 13:42:32 +0800

    update

--- js/common.js
+++ js/common.js
@@ -1,17 +1,19 @@
 
 $(function(){
-    $(".main > li").hover(function (){  
-           if($(this).children('ul')){
-               var elemCount=$(this).children('ul').children('li').length,
-                   elem=$(this).children('ul').children('li');
-               for(var i=0;i<elemCount;i++){
-                   var currentElem=elem.eq(i);
-                   var vari=.3 * (i+1);
-                   currentElem.css('animation-delay',+ vari +'s')
-               }
-           }  
-       }); 
-        $(".main > li ul").mouseout(function (){  
-           vari=1; 
-       }); 
+    $('.main li').mouseenter(function(){ 
+        if($(this).children('ul')){
+            $(this).children('ul').css('display','block'); //改变初始化样式
+           var elemCount=$(this).children('ul').children('li').length,
+               elem=$(this).children('ul').children('li');
+            
+           for(var i=0;i<elemCount;i++){
+               var currentElem=elem.eq(i);
+               var vari=.3 * (i+1);
+               currentElem.css('animation-delay',+ vari +'s')
+           }
+       }
+    }).mouseleave(function(){
+        vari=1; //重置变量
+        $(this).children('ul').css('display','none');//初始化
+    });  
    })
